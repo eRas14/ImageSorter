2f474ecea1d2bec70a1ff66a3b146866b9c4f72c
Aibek Urazov (HomePC) <ibek-94@mail.ru>
2024-07-23 22:35:26 +0500

    fix 1.6 добавлены коментарии для себя любимого в будущем

--- ImageSorter.py
+++ ImageSorter.py
@@ -25,23 +25,28 @@ class ImageSorter:
         self.root.configure(background='black')
         self.root.title("Image Sorter by eRas")
         self.label = Label(self.root)
-        self.label.pack(expand=True)
-
+        self.label.pack(expand=True) #expend=True - отвечает за отображение фото по центру
+        
+        #Словарь где хранятся расположение кнопок
         user_buttons = {1: 'left', 2: 'right', 3: 'bottom', 4: 'top'}
-        buttons = []
 
+        #Пусто словрь для создания кнопок
+        buttons = []
+        
+        #В этом цикле создаются кнопки, и добавляются в список buttons, после идет распоковка, где берется значение из user_buttons (располождение кнопки)
         for i in range(len(folder_names)):
             btn = Button(self.root, text=folder_names[i], command=lambda i=i: self.move_to_folder(self.folders[i]))
             buttons.append(btn)
             btn.pack(side=user_buttons.get(i+1))
 
+        #Папки по умолчанию на кнопках, отсюда вывод -- > забинжены только 4 стрелки
         self.load_image()
-        self.root.bind("<Left>", lambda event: self.move_to_folder(self.folders[0]))  #Папки по умолчанию на кнопках
+        self.root.bind("<Left>", lambda event: self.move_to_folder(self.folders[0]))  
         self.root.bind("<Right>", lambda event: self.move_to_folder(self.folders[1])) 
         self.root.bind("<Down>", lambda event: self.move_to_folder(self.folders[2])) 
         self.root.bind("<Up>", lambda event: self.move_to_folder(self.folders[3])) 
         self.root.mainloop()
-
+    #Загрузка фото в окно
     def load_image(self):
         if self.index < len(self.images):
             image_path = os.path.join(self.folder_path, self.images[self.index])
@@ -52,7 +57,7 @@ class ImageSorter:
         else:
             self.label.config(text="Все изображения отсортированы!")
             self.label.image = None
-
+    #Перемещаем фото
     def move_to_folder(self, target_folder):
         if self.index < len(self.images):
             source_image_path = os.path.join(self.folder_path, self.images[self.index])
